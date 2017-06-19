226471bd5f8e4fceff03adf5ad52132e6b6b2509
Filipe Moreira <code@filipemoreira.com>
2017-06-19 09:46:53 +0100

    Fix: add correct domain and CNAME file

--- scripts/publish_toghpages.sh
+++ scripts/publish_toghpages.sh
@@ -25,6 +25,9 @@ rm -rf public/*
 echo "Generating site"
 hugo
 
+echo "Adding CNAME file"
+rm CNAME
+echo filipeamoreira.com > CNAME
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 git push origin gh-pages
